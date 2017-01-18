add57210140aba1835e7403044d1e52886895890
zilonghe <495309675@qq.com>
2017-01-18 14:50:18 +0800

    Initial Commit

---
+++ QuickSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickSearch
+//
+//  Created by 何子龙 on 18/01/2017.
+//  Copyright © 2017 何子龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
